b1da3c5d887ba16a7d6af71b92cdeff032d6a218
SeolDongWon <seol.east@gmail.com>
2023-12-22 18:10:50 +0900

    no message

--- _seolProject/sp_script.js
+++ _seolProject/sp_script.js
@@ -8,11 +8,11 @@ function screanCheck() {
     document.getElementById('containerHight').innerHTML =
         '<h1>containerHight : ' + containDiv.clientHeight + '</h1>';
 
-    let maintab2Div = document.getElementById('maintab2');
-    document.getElementById('maintab2Width').innerHTML =
-        '<h1>maintab2Width : ' + maintab2Div.clientWidth + '</h1>';
-    document.getElementById('maintab2Hight').innerHTML =
-        '<h1>maintab2Hight : ' + maintab2Div.clientHeight + '</h1>';
+    // let maintab2Div = document.getElementById('maintab2');
+    // document.getElementById('maintab2Width').innerHTML =
+    //     '<h1>maintab2Width : ' + maintab2Div.clientWidth + '</h1>';
+    // document.getElementById('maintab2Hight').innerHTML =
+    //     '<h1>maintab2Hight : ' + maintab2Div.clientHeight + '</h1>';
 
     if (containDiv.clientWidth < 1140) {
         document.getElementById('aside').style.display = "none";
